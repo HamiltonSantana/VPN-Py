049cb8f044bbb03c14a56322c65286ec90b764c4
HamiltonSantana <hamilton.santana@outlook.com>
2017-02-06 09:23:33 -0200

    Excluído "teste.py"

--- teste.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def main():
-	arq = open('C:\\PROGRA~2\\CISCOS~1\\VPNCLI~1\\login.data', 'r')
-	os.system('C:\\PROGRA~2\\CISCOS~1\\VPNCLI~1\\vpnclient.exe '+ arq.readline())
-
-if __name__ == '__main__':
-	main()
